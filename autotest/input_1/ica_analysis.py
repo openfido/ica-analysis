13e6d6dde6e7d9e18575a10372166043888b6a25
David P. Chassin <dchassin@slac.stanford.edu>
2021-01-10 18:57:58 -0800

    Fix handling of phases

--- autotest/input_1/ica_analysis.py
+++ autotest/input_1/ica_analysis.py
@@ -68,12 +68,13 @@ def on_init(t):
     for objname in object_list:
         classname = gridlabd.get_value(objname,"class")
         if classname in target_properties.keys():
-            pattern = target_properties[classname]
-            classdata = gridlabd.get_class(classname)
-            for propname in classdata.keys():
-                if re.match(pattern,propname):
-                    link_property(objname,propname,nonzero=False)
-    print(property_list.keys())
+            objdata = gridlabd.get_object(objname)
+            if "phases" in objdata.keys():
+                classdata = gridlabd.get_class(classname)
+                pattern = target_properties[classname].replace("[ABC]",f"[{objdata['phases']}]")
+                for propname in classdata.keys():
+                    if re.match(pattern,propname):
+                        link_property(objname,propname,nonzero=False)
     return True
 
 def on_sync(t):
@@ -82,46 +83,52 @@ def on_sync(t):
     global limit_list
 
     dt = datetime.datetime.fromtimestamp(t)
-    print(f"*** onsync({dt}) ***")
+    gridlabd.debug(f"*** onsync({dt}) ***")
     violation_active = int(gridlabd.get_global("powerflow::violation_active"))
     if violation_active:
-        print(f"{dt}: violation detected (violation_active={violation_active})",flush=True)
+        gridlabd.debug(f"{dt}: violation detected (violation_active={violation_active})")
 
     done = None
     if property_list:
         objname = list(property_list.keys())[0]
-        print(f"{dt}: updating {objname}",flush=True)
+        gridlabd.debug(f"{dt}: updating {objname}")
         proplist = property_list[objname]
-        for propname, specs in proplist.items():
-            prop = specs[0]
-            base = specs[1]
-            if base.real == 0.0:
-                value = prop.get_value() - complex(delta,delta*reactive_ratio)
-            else:
-                value = prop.get_value() - base/base.real*delta
-            if not violation_active:
-                print(f"{dt}: updating {objname}.{propname} = {value}",flush=True)
-                prop.set_value(value)
-                load_limit = base - value
-                limit_list[objname][propname] = {"timestamp":t, "real":load_limit.real, "reactive":load_limit.imag}
-            else:
-                print(f"{dt}: resetting {objname}.{propname} to base {base}")
-                prop.set_value(base)
-                if violation_active:
-                    done = objname
+        if proplist:
+            for propname, specs in proplist.items():
+                prop = specs[0]
+                base = specs[1]
+                if base.real == 0.0:
+                    value = prop.get_value() - complex(delta,delta*reactive_ratio)
+                else:
+                    value = prop.get_value() - base/base.real*delta
+                if not violation_active:
+                    gridlabd.debug(f"{dt}: updating {objname}.{propname} = {value}")
+                    prop.set_value(value)
+                    load_limit = base - value
+                    limit_list[objname][propname] = {"timestamp":t, "real":load_limit.real, "reactive":load_limit.imag}
+                else:
+                    gridlabd.debug(f"{dt}: resetting {objname}.{propname} to base {base}")
+                    prop.set_value(base)
+                    if violation_active:
+                        done = objname
+        else:
+            done = objname
         if done:
-            print(f"{dt}: finished with {objname}",flush=True)
-            del property_list[objname]
+            gridlabd.debug(f"{dt}: finished with {objname}")
+            if proplist == {}:
+                del property_list[objname]
+            else:
+                property_list[objname] = {}
             if property_list:
-                print(f"{dt}: next is {list(property_list.keys())[0]}",flush=True)
+                gridlabd.debug(f"{dt}: next is {list(property_list.keys())[0]}")
         if violation_active:
-            print(f"{dt}: clearing violation",flush=True)
+            gridlabd.debug(f"{dt}: clearing violation")
             gridlabd.set_global("powerflow::violation_active","0")
         tt = t+60
-        print(f"updating to {datetime.datetime.fromtimestamp(tt)}",flush=True)
+        gridlabd.debug(f"updating to {datetime.datetime.fromtimestamp(tt)}")
         return tt
     else:
-        print(f"updating to NEVER",flush=True)
+        gridlabd.debug(f"updating to NEVER")
         return gridlabd.NEVER
 
 def on_term(t):
@@ -129,11 +136,13 @@ def on_term(t):
     global output_folder
     global limit_list
 
-    with open(f"{output_folder}/solar_limits.csv","w") as fh:
+    with open(f"{output_folder}/solar_capacity.csv","w") as fh:
         writer = csv.writer(fh)
-        writer.writerow(["object","property","timestamp","real","reactive"])
+        writer.writerow(["load","solar_capacity[kW]"])
         for objname, property_data in limit_list.items():
+            power = 0.0
             for propname, data in property_data.items():
-                writer.writerow([objname,propname,datetime.datetime.fromtimestamp(data["timestamp"]),data["real"],data["reactive"]])
+                power += data["real"]/1000.0   
+            writer.writerow([objname,round(power,1)])
     return None
 
