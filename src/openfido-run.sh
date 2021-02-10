245d00f5c35057db95114c6e25d32ac0907f23f3
David P. Chassin <dchassin@slac.stanford.edu>
2021-02-10 13:04:59 -0800

    Update openfido-run.sh

--- src/openfido-run.sh
+++ src/openfido-run.sh
@@ -9,7 +9,7 @@ cp ${OPENFIDO_INPUT}/* .
 for MODEL in $(ls -1 ${MODELNAME:-*.glm}); do
 	if [ -z "${MODEL}" -a ! -f "${MODEL}" ]; then
 		error $E_NOTFOUND "file '${MODEL}' is not found"
-	elif [ "${MODEL}" != "${GLMCONFIG}" -a "${MODEL}" != "${TEMPLATE}".glm -a "${MODEL}" != "${GLMRECORD}"  ]; then
+	elif [ "${MODEL}" != "${GLMCONFIG}" -a "${MODEL}" != "${TEMPLATE}.glm" -a "${MODEL}" != "${GLMRECORD}"  ]; then
 		export GLPATH="$(gridlabd --version=install)/share/gridlabd/template/$ORGANIZATION"
 		if [ -d "${GLPATH}/${TEMPLATE}" ]; then
 			export GLPATH="$(gridlabd --version=install)/share/gridlabd/template/$ORGANIZATION/${TEMPLATE}"
