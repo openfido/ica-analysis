620fab803405d7d570ed5cc5e9cb7b766de43a49
David P. Chassin <dchassin@slac.stanford.edu>
2021-02-10 12:45:45 -0800

    Update openfido-run.sh

--- src/openfido-run.sh
+++ src/openfido-run.sh
@@ -9,14 +9,14 @@ cp ${OPENFIDO_INPUT}/* .
 for MODEL in $(ls -1 ${MODELNAME:-*.glm}); do
 	if [ -z "${MODEL}" -a ! -f "${MODEL}" ]; then
 		error $E_NOTFOUND "file '${MODEL}' is not found"
-	elif [ "${MODEL}" != "${GLMCONFIG}" -a "${MODEL}" != "${TEMPLATE}" -a "${MODEL}" != "${GLMRECORD}"  ]; then
+	elif [ "${MODEL}" != "${GLMCONFIG}" -a "${MODEL}" != "${TEMPLATE}".glm -a "${MODEL}" != "${GLMRECORD}"  ]; then
 		export GLPATH="$(gridlabd --version=install)/share/gridlabd/template/$ORGANIZATION"
 		if [ -d "${GLPATH}/${TEMPLATE}" ]; then
 			export GLPATH="$(gridlabd --version=install)/share/gridlabd/template/$ORGANIZATION/${TEMPLATE}"
 		elif [ ! -f "${GLPATH}/${TEMPLATE}.glm" ]; then
 			error $E_NOTFOUND "template '${TEMPLATE}' is not found in '${GLPATH}"
 		fi
-		cp "$GLPATH/$TEMPLATE.py" .
-		gridlabd -D OUTPUT=${OPENFIDO_OUTPUT} ${GLMCONFIG} "${MODEL}" ${GLMRECORD} "${GLPATH}/${TEMPLATE}.glm"
+		cp "$GLPATH/$TEMPLATE".* .
+		gridlabd -D OUTPUT=${OPENFIDO_OUTPUT} ${GLMCONFIG} "${MODEL}" ${GLMRECORD} ${TEMPLATE}.glm
 	fi
 done
